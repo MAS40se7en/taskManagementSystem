811d5177106bce2e90cc68520ef7a7063e8eaf7b
MAS40se7en <manir.saker@gmail.com>
2024-11-29 19:53:28 +0100

    updated some post functions

--- src/routes/protected/messages/convo/[id]/+server.ts
+++ src/routes/protected/messages/convo/[id]/+server.ts
@@ -1,7 +1,12 @@
-<<<<<<< HEAD
-// +server.ts
 import { prisma } from "$lib/prisma";
 import { json } from "@sveltejs/kit";
+
+import admin from 'firebase-admin';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const serviceAccountPath = join(process.cwd(), 'src/lib/server/serviceAccountKey.json');
+const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf8'));
 
 export async function GET({ params, locals }) {
   const { user } = locals;
@@ -84,114 +89,55 @@ export async function POST({request, locals}) {
       }
     });
 
-    return json({ newMessage }, { status: 201 });
-  } catch (error) {
-    return json({ message: 'Failed to send message', error}, { status: 500 });
-  }
-}
-
-export async function DELETE({ params }) {
-  const { id } = params;
-  const conversationId = parseInt(id, 10);
-
-  if (isNaN(conversationId)) {
-    return json({ message: 'Invalid ID' }, { status: 400 });
-  }
-
-  try {
-    await prisma.conversation.delete({
-      where: { id: conversationId },
-    });
-
-    return json({ message: 'Conversation deleted successfully' }, { status: 204 });
-  } catch (error) {
-    return json({ message: 'Failed to delete conversation', error }, { status: 500 });
-  }
-=======
-// +server.ts
-import { prisma } from "$lib/prisma";
-import { json } from "@sveltejs/kit";
-
-export async function GET({ params, locals }) {
-  const { user } = locals;
-
-  if (!user) {
-      return new Response("User not logged in", { status: 401 });
-  }
-
-  const { id } = params;
-  const conversationId = parseInt(id, 10);
-
-  if (isNaN(conversationId)) {
-    return json({ message: 'Invalid ID' }, { status: 400 });
-  }
-
-  try {
     const conversation = await prisma.conversation.findUnique({
-      where: { id: conversationId },
-      include: {
-          participants: {
-              select: {
-                  id: true,
-                  name: true,
-                  email: true,
-                  image: true,
-              },
-          },
-          messages: {
-              orderBy: {
-                  sentAt: 'asc',
-              },
-          },
-      }
-    });
-
-    if (!conversation) {
-      return json({ message: 'Conversation not found' }, { status: 404 });
-    }
-
-    // Extract participants' IDs and names to pass them in the URL
-    const participantIds = conversation.participants.map((participant) => participant.id);
-
-    return json({ conversation, participantIds, user }, { status: 200 });
-  } catch (error) {
-    return json({ message: 'Internal server error' }, { status: 500 });
-  }
-}
-
-
-export async function POST({request, locals}) {
-  const user = locals.user;
-
-  if (!user) {
-    return json({ message: 'Unauthorized' }, { status: 401 });
-  }
-
-  try {
-    const { conversationId, content } = await request.json();
-
-    if (!conversationId || !content) {
-      return json({ message: 'Missing required fields' }, { status: 400 });
-    }
-
-    const conversationIdInt = parseInt(conversationId, 10);
-
-    const newMessage = await prisma.message.create({
-      data: {
-        content,
-        senderId: user.id,
-        conversationId: conversationIdInt,
+      where: {
+        id: conversationIdInt,
       },
       include: {
-        sender: {
+        participants: {
           select: {
-            id: true,
-            name: true,
-            image: true,
+              id: true,
+              name: true,
+              email: true,
+              image: true,
           },
-        },
+      },
       }
-    });
+    })
+
+    const receivers = conversation?.participants.filter((participant) => participant.id !== user.id)
+
+    if (newMessage) {
+      if (receivers) {
+        receivers.map(async (receiver) => {
+          const receivingUser = await prisma.user.findUnique({
+            where: {
+              id: receiver.id
+            }
+          })
+  
+          if (receivingUser && receivingUser.deviceFcmToken) {
+            const payload = {
+              notification: {
+                title: `from ${user.name}`,
+                body: `${newMessage.content}`
+              },
+              token: receivingUser.deviceFcmToken
+            }
+
+            if (payload.token) {
+              try {
+                const response = await admin.messaging().send(payload);
+                console.log(`Notification sent to user ${user.name}:`, response);
+            } catch (error) {
+                console.error(`Error sending notification for message ${newMessage.content}:`, error);
+            }
+            }
+          }
+          
+        })
+      }
+    }
 
     return json({ newMessage }, { status: 201 });
   } catch (error) {
@@ -216,5 +162,4 @@ export async function DELETE({ params }) {
   } catch (error) {
     return json({ message: 'Failed to delete conversation', error }, { status: 500 });
   }
->>>>>>> 2689b32a279db71525fba4df4c3d46aae9075f8c
 }
