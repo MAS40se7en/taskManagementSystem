88c250d426791ea39e804de838ae460ce75428fd
MAS40se7en <manir.saker@gmail.com>
2024-12-18 23:33:48 +0100

    'small fix'

--- src/routes/protected/mobile-admin/+page.svelte
+++ src/routes/protected/mobile-admin/+page.svelte
@@ -1,8 +1,9 @@
-<<<<<<< HEAD
+
 <div class="w-4/5 mx-auto h-screen place-content-center">
     <p class="font-extrabold text-wrap text-4xl">It's better to access the admin layout on a desktop</p>
-=======
+</div>
+
 <div class="w-4/5 mx-auto h-screen place-content-center">
     <p class="font-extrabold text-wrap text-4xl">It's better to access the admin layout on a desktop</p>
->>>>>>> 2689b32a279db71525fba4df4c3d46aae9075f8c
+
 </div>
