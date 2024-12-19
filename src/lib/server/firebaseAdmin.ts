9882f1238a79e0eeb1cc16385d922f7bb33d0d97
MAS40se7en <manir.saker@gmail.com>
2024-12-19 01:39:22 +0100

    'making changes'

--- src/lib/server/firebaseAdmin.ts
+++ src/lib/server/firebaseAdmin.ts
@@ -1,9 +1,4 @@
 import admin from 'firebase-admin';
-//import { readFileSync } from 'fs';
-//import { join } from 'path';
-
-//const serviceAccountPath = join(process.cwd(), '/src/lib/server/serviceAccountKey.json');
-//const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf8'));
 
 const serviceAccountKey = JSON.parse(Buffer.from(process.env.SERVICE_ACCOUNT_KEY, 'base64').toString('utf-8'))
 if (!admin.apps.length) {
