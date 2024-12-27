79ece9569c46c5a9576d086bbc19a489f8641ffc
MAS40se7en <manir.saker@gmail.com>
2024-12-27 21:19:38 +0100

    ''

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,37 +23,38 @@
 	onMount(async () => {
 		App.addListener('appUrlOpen', async (event) => {
 			const url = new URL(event.url);
+			
 
-			if (url.protocol === 'https:' && url.host === 'task-management-system-steel.vercel.app') {
-				const urlParams = new URLSearchParams(window.location.search);
-				code = urlParams.get('code');
-				state = urlParams.get('state');
+			const urlParams = new URLSearchParams(window.location.search);
+			code = urlParams.get('code');
+			state = urlParams.get('state');
 
-				if (code && state) {
-					loading = true;
-					try {
-						const response = await fetch('/api/google/callback', {
-							method: 'POST',
-							body: JSON.stringify({
-								code,
-								state
-							}),
-							headers: {
-								'Content-Type': 'application/json'
-							}
-						});
+			console.log(code, state);
 
-						const data = await response.json();
-
-						if (response.ok) {
-							loading = false;
-							goto('/protected');
-						} else {
-							console.error('Authenticatiopn failed: ', data.error);
+			if (code && state) {
+				loading = true;
+				try {
+					const response = await fetch('/api/google/callback', {
+						method: 'POST',
+						body: JSON.stringify({
+							code,
+							state
+						}),
+						headers: {
+							'Content-Type': 'application/json'
 						}
-					} catch (error) {
-						console.error('Error handling authentication: ', error);
+					});
+
+					const data = await response.json();
+
+					if (response.ok) {
+						loading = false;
+						goto('/protected');
+					} else {
+						console.error('Authenticatiopn failed: ', data.error);
 					}
+				} catch (error) {
+					console.error('Error handling authentication: ', error);
 				}
 			}
 		});
@@ -92,9 +93,11 @@
 </script>
 
 {#if loading}
-	<div class="w-full top-0 right-0 left-0 bottom-0 absolute rounded-3xl backdrop-blur-sm z-50 flex flex-col place-items-center justify-center">
+	<div
+		class="w-full top-0 right-0 left-0 bottom-0 absolute rounded-3xl backdrop-blur-sm z-50 flex flex-col place-items-center justify-center"
+	>
 		<Icon icon="line-md:loading-twotone-loop" class="w-24 h-24" />
-        <p class="text-xl font-semibold">Loading your data from google!</p>
+		<p class="text-xl font-semibold">Loading your data from google!</p>
 	</div>
 {/if}
 
